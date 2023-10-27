e76598b5f0f17ff44d097e5d2859282a55c38f99
TheWhatis <anton-gogo@mail.ru>
2023-10-28 01:16:59 +0700

    Создал трейт для реализации методов ICast

---
+++ src/cast/TCast.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Файл с трейтом, реализующим
+ * интерфейс `ICast`
+ *
+ * PHP version 8
+ *
+ * @category Cast
+ * @package  PhpCast
+ * @author   Whatis <anton-gogo@mail.ru>
+ * @license  unlicense
+ * @link     https://github.com/TheWhatis/PhpCast
+ */
+
+namespace Whatis\PhpCast;
+
+/**
+ * Трейт, реализующий
+ * `ICast`
+ *
+ * PHP version 8
+ *
+ * @category Cast
+ * @package  PhpCast
+ * @author   Whatis <anton-gogo@mail.ru>
+ * @license  unlicense
+ * @link     https://github.com/TheWhatis/PhpCast
+ */
+trait TCast
+{
+    /**
+     * Аргументы
+     *
+     * @var array
+     */
+    public array $arguments;
+
+    /**
+     * Иницилизация правила
+     *
+     * @param array $arguments Приоритет
+     */
+    public function __construct(array $arguments = []) {
+        $this->arguments = $this->parseArguments($arguments);
+    }
+
+    /**
+     * Отправлять ли исключительно значения,
+     * которые были пропущены
+     *
+     * @return bool
+     */
+    public static function onlyMissed(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Обработать переданные аргументы
+     *
+     * @param array $arguments Аргументы
+     *
+     * @return array Обработанные аргументы
+     */
+    abstract protected function parseArguments(array $arguments): array;
+}
